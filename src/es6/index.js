71fa76a6b7a8dcfe19404e4b7a4c5d4fe845a4a3
Mario <modulor@hotmail.es>
2020-09-14 10:42:23 -0500

    examples for: default params and concat a string

---
+++ src/es6/index.js
@@ -0,0 +1,12 @@
+// default params
+
+function foo(name = 'John', age = 20, countryCode = 'NZ') {
+  console.log(name, age, countryCode);
+}
+
+// concat
+
+let firstName = 'Peter';
+let lastName = 'McClam';
+let fullName = `${firstName} ${lasttName}`;
+
